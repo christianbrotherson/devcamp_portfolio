47433ce4a23a9a721d8219467dcd100dfc53a21b
Christian Brotherson <christian,brotherson@gmail,com>
2017-03-13 15:57:15 -0600

    Implemented partials for blog, font awesome, and pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
 	end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Christian Brotherson', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright  'Christian Brotherson', 'All rights reserved'
   end
 end
