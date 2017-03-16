fa59bae78d6a71f77428633f0c3234b910bb31b2
Christian Brotherson <christian,brotherson@gmail,com>
2017-03-15 19:46:10 -0600

    finished carrierwave installation

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,12 +1,11 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   
-
-	def index
-		@portfolio_items = Portfolio.by_position
-	end
+  def index
+    @portfolio_items = Portfolio.by_position
+  end
 
   def sort
     params[:order].each do |key, value|
@@ -20,17 +19,17 @@ class PortfoliosController < ApplicationController
     @angular_portfolio_items = Portfolio.angular
   end
 
-	def new
-		@portfolio_item = Portfolio.new
+  def new
+    @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
-	end
+  end
 
-	def create
+  def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live!' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -43,7 +42,7 @@ class PortfoliosController < ApplicationController
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio has been updated!' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -54,34 +53,28 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-  	# Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-  	# Destroy/Delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
 
-  	# Redirect
+    # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Your portfolio has been deleted!' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
 
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
-                                      :body, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
                                       :main_image,
                                       :thumb_image,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
 
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-
-
-
-
 end
